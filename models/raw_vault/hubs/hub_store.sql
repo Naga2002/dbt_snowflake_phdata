e7f57dc313e161115721a26d57f8022b5e61a644
Nathan Mott <NMOTT@phdata.io>
2023-11-06 13:37:49 +0000

    add stores base, stg, hub, and sat models

---
+++ models/raw_vault/hubs/hub_store.sql
@@ -0,0 +1,8 @@
+{%- set source_model = "stg_storess" -%}
+{%- set src_pk = "STORE_HK" -%}
+{%- set src_nk = "store_id" -%}
+{%- set src_ldts = "LOAD_DATETIME" -%}
+{%- set src_source = "RECORD_SOURCE" -%}
+
+{{ automate_dv.hub(src_pk=src_pk, src_nk=src_nk, src_ldts=src_ldts,
+                   src_source=src_source, source_model=source_model) }}
