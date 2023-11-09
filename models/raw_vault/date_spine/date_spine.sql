02e9fe0fa206409f3eb7c6b491a9d5dc768c1f76
Nathan Mott <NMOTT@phdata.io>
2023-11-09 13:52:39 +0000

    add pit_authors

--- models/raw_vault/date_spine/date_spine.sql
+++ models/raw_vault/date_spine/date_spine.sql
@@ -1,8 +1,8 @@
 {{ config(materialized='table') }}
 
 {%- set datepart = "day" -%}
-{%- set start_date = "TO_DATE('1992/01/01', 'yyyy/mm/dd')" -%}
-{%- set end_date = "TO_DATE('1995/01/01', 'yyyy/mm/dd')" -%}
+{%- set start_date = "TO_DATE('2023/11/01', 'yyyy/mm/dd')" -%}
+{%- set end_date = "TO_DATE('2023/12/01', 'yyyy/mm/dd')" -%}
 
 WITH as_of_date AS (
     {{ dbt_utils.date_spine(datepart=datepart, 
